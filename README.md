771986a5698ab7888c0e403c82f9efd7e3da07ab
Janne Spijkervet <janne.spijkervet@hotmail.com>
2018-05-23 14:54:59 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ros_storage
+
+## Installation
+
+See https://gitlab.com/uva-robotics/boilerplate for installation instructions.If you are running Ubuntu x64:
+```
+sudo apt-get install python-pip python-dev
+```
+
+## ROS Node
+
+```
+rosrun boiplerplate_pkg node
+```
+
+## Scripts
+
+```
+rosrun boiplerplate_pkg script
+```
---
+++ src/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import rospy
+from std_msgs.msg import String
+from diagnostic_msgs.msg import DiagnosticArray
+import MySQLdb
+import random
+
+db = MySQLdb.connect(host="localhost",
+                     user="root",
+                     passwd="root",
+                     db="test")
+
+
+def callback(data):
+    battery_level = data.status[18].values[0].value
+    temperature = data.status[1].values[0].value
+    cur = db.cursor()
+    query = "INSERT INTO test_data (`battery_level`, `temperature`) VALUES ({}, {})".format(battery_level, temperature)
+    cur.execute(query)
+    db.commit()
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("/diagnostics", DiagnosticArray, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+        db.close()
